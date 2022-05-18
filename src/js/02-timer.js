cadf8e8843c0b216fd69b14e1e98ac64d4a807b6
Kate Yankovenko <k.yankovenko@ukr.net>
2022-05-18 22:06:36 +0200

    added imports

---
+++ src/js/02-timer.js
@@ -0,0 +1,13 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+//replacing of standart alert log usage
+import Notiflix from 'notiflix';
+//all modules
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,13 @@
+//replacing of standart alert log usage
+import Notiflix from 'notiflix';
+//all modules
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
