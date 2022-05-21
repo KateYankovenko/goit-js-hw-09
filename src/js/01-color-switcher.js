4863a6d777bb6e99cc1dd7854452355b0325667d
Kate Yankovenko <k.yankovenko@ukr.net>
2022-05-21 13:57:58 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,7 @@
+const startBtn = document.querySelector("#data-start");
+const stopBtn = document.querySelector("#data-stop");
+console.log(startBtn)
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
